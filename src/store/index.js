3aae0d75fbe9b40187bb0b0f6ff8b57daf98c84d
유주현 <gus6359@gmail.com>
2022-09-20 05:34:02 +0900

    Update: Add store slice the account

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import globalReducer from "./slices/global";
+import accountReducer from "./slices/account";
 
 export default configureStore({
   reducer: {
     global: globalReducer,
+    account: accountReducer,
   }
 });
---
+++ src/store/slices/account.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: null,
+  accessToken: null,
+  refreshToken: null,
+};
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    setEmail: (state, action) => { state.email = action.payload; },
+    setAccessToken: (state, action) => { state.accessToken = action.payload; },
+    setRefreshToken: (state, action) => { state.refreshToken = action.payload; },
+  }
+});
+
+export const { setEmail, setAccessToken, setRefreshToken, } = accountSlice.actions;
+export default accountSlice.reducer;
